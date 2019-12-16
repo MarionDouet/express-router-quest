8eafaa87e3206bd126ff9ab9307829352df37d5c
Marion Douet <squalaladraw@gmail.com>
2019-12-16 14:39:13 +0100

    ajout route tag

---
+++ routes/tags.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fakesTags = require('../data/tags');
+const fakePosts = require('../data/posts');
+const router = express.Router({mergeParams: true});
+
+router.get('/', (req, res) => {
+  res.json(fakesTags)
+})
+
+
+router.get('/:tagId/posts', (req, res) => {
+  const tagId = Number(req.params.tagId)
+  const tag = fakePosts.find(tag => tag.id === tagId);
+  if(!tag)
+    return res.status(404).json({
+    error: 'Post not found',
+  })
+ 
+  const filPost = fakePosts.filter((post) => post.tag_ids.includes(tagId));
+
+
+  res.json(filPost);
+});
+
+
+module.exports = router;
